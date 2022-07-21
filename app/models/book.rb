1514a16be7796ab10f55e47a5675fcb828265774
kashif tufail <kashif.tufail17@gmail.com>
2022-07-21 23:57:14 +0500

    specs for book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 3 , maximum: 20 }
+  validates :description, presence: true, length: { minimum: 15 , maximum: 500 }
+
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,9 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  it 'is valid with a valid attributes'
-  it 'is not valid without title'
-  it 'is not valid without description'
-  it 'is valid with valid title length'
-  it 'is valid with valid description length'
+  it 'is valid with a valid attributes' do
+    expect(Book.new).to be_valid
+  end
+  it 'is not valid without title' do
+    book = Book.new(title: nil)
+    expect(book).not_to be_valid
+  end
+  it 'is not valid without description' do
+    book = Book.new(description: nil)
+    expect(book).not_to be_valid
+  end
+  it 'is valid with valid title length' do
+    book = Book.new(title: 'lovely rspec',description: 'lovely way to solid testing with rspec')
+    expect(book).to be_valid
+  end
+  it 'is valid with valid description length' do
+    book = Book.new(title: 'lovely rspec',description: 'lovely way to solid testing with rspec')
+    expect(book).to be_valid
+  end
+
+
+
+  
+
+
 end

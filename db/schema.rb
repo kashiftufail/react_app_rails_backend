4a6a5aa4052e4290e6403bb690918a16b75e2d74
kashif tufail <kashif.tufail17@gmail.com>
2022-07-22 22:54:12 +0500

    update db according pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_07_21_173113) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "description"
